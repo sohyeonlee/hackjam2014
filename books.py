26dab19df5e9582e50c2b501e4514c5a4a6636c7
Sohyeon Lee <sohyeonlee@berkeley.edu>
2014-09-27 19:02:12 -0700

    updated register

--- books.py
+++ books.py
@@ -1,59 +1,39 @@
-from flask import Flask, render_template, session, redirect, url_for
-from flask.ext.login import LoginManager, current_user
-from flask.ext.login import login_user, logout_user
-
+from flask import Flask, render_template, session, redirect, url_for, request
+from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from models import User, RegistrationForm
 
 app = Flask(__name__)
-login_manager = LoginManager()
-login_manager.setup_app(app)
-#login_manager.login_view = 'login'
+app.config['SECRET_KEY'] = '$\xf8\x81\xb2[J_l\x0c\x18\x86^p\xc9\xba\xb3\x88_\x03\xc8\x9dz\xf4/'
+db = SQLAlchemy(app)
+engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,autoflush=False,bind=engine))
 
 @app.route('/')
 def index():
-    if 'username' in session:
-        return "welcome"
-    return "Please login"
-    
-#
-# @app.route('/login', methods=['POST','GET'])
-# def login():
-#     error = None
-#     if request.method == 'POST':
-#         return "login"
-#     else:
-#         return "signupform"
+    return render_template('index.html')
 
-@login_manager.user_loader
-def load_user(user_id):
-    """Flask-Login hook to load a User instance from ID."""
-    return db.session.query(User).get(user_id)
-
+# route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated():
-        return redirect(url_for('appointment_list'))
-        form = LoginForm(request.form)
-        error = None
-
-    if request.method == 'POST' and form.validate():
-        email = form.username.data.lower().strip()
-        password = form.password.data.lower().strip()
-        user, authenticated = \
-        User.authenticate(db.session.query, email,password)
-    if authenticated:
-        login_user(user)
-        return redirect(url_for('appointment_list'))
-    else:
-        error = 'Incorrect username or password.'
-        # return render_template('user/login.html', form=form, error=error)
-        return "error"
-
-@app.route('/logout/')
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-if __name__ == '__main__':
-    app.run(debug=True)
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('register'))
+    return render_template('login.html', error=error)
     
-# set the secret key.  keep this really secret:
-app.secret_key = '$\xf8\x81\xb2[J_l\x0c\x18\x86^p\xc9\xba\xb3\x88_\x03\xc8\x9dz\xf4/'
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data, form.password.data)
+        print("i m here")
+        db_session.add(user)
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)  
+      
+if __name__ == '__main__':
+     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
+from wtforms import Form, BooleanField, TextField, PasswordField, validators
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+ 
+class User(db.Model):
+  __tablename__ = 'users'
+  uid = db.Column(db.Integer, primary_key = True)
+  username = db.Column(db.String(100))
+  email = db.Column(db.String(120), unique=True)
+  pwdhash = db.Column(db.String(54))
+   
+  def __init__(self, username, email, password):
+    self.username = username.title()
+    self.email = email.lower()
+    self.set_password(password)
+     
+  def set_password(self, password):
+    self.pwdhash = generate_password_hash(password)
+   
+  def check_password(self, password):
+    return check_password_hash(self.pwdhash, password)
+    
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email = TextField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
